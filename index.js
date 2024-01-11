83c3a70ad1efbfa55fba12a73706886b53db1f95
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-01-11 20:58:51 -0300

    feat: adding tasks array to localStorage

--- index.js
+++ index.js
@@ -17,3 +17,36 @@ function createNewTask(e) {
 
   taskPriority()
 }
+
+window.addEventListener('load', () => {
+  tasks = JSON.parse(localStorage.getItem('tasks')) || []; //global variable
+  const nameOwner = document.querySelector('#owner-name');
+  const helloUsername = document.querySelector('.username');
+  const username = localStorage.getItem('username') || '';
+  const newTaskForm = document.querySelector('#tasks-form');
+
+  nameOwner.addEventListener('change', e => {
+    localStorage.setItem('username', e.target.value)
+    helloUsername.textContent = "de " + e.target.value;
+  })
+  nameOwner.value = username;
+  helloUsername.textContent = "de " + username;
+
+  newTaskForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    //creating tasks array
+    const task = {
+      name: e.target.elements.task.value,
+      priority: e.target.elements.priority.checked,
+      done: false,
+      createdAt: new Date().getTime()
+    }
+    //adding a task to the array
+    tasks.push(task);
+
+    //saving the array localStorage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    e.target.reset();
+  })
+})
