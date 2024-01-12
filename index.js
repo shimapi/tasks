87b95c3b45bfbc77a70daa5625763dbf0598fd6c
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-01-12 11:27:46 -0300

    feat: adding tasks

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 
 function taskPriority() {
-  const priority = document.getElementsByName('priority');
-  const priorityChecked = document.getElementsByName('priority').checked;
-  console.log(priorityChecked)
+  const priorityOptions = document.getElementsByName('priority');
+  let selectedPriority = '';
 
-  for (let i = 0; i < priority.length; i++) {
-    console.log('priority[i].value', priority[i].value);
+  for (let i = 0; i < priorityOptions.length; i++) {
+    if (priorityOptions[i].checked) {
+      selectedPriority = priorityOptions[i].value;
+      break;
+    }
   }
+
+  console.log('Selected Priority:', selectedPriority);
+  return selectedPriority;
 }
+
 
 function createNewTask(e) {
   e.preventDefault();
   const taskName = document.querySelector('#task-name').value;
+  const taskPriorityValue = taskPriority();
 
-  console.log(taskName);
+  console.log('Task Name:', taskName);
+  console.log('Task Priority:', taskPriorityValue);
 
-  taskPriority()
+  //creating tasks array
+  const task = {
+    name: taskName,
+    priority: taskPriorityValue,
+    done: false,
+    createdAt: new Date().getTime()
+  }
+  //adding a task to the array
+  tasks.push(task);
+
+  //saving the array localStorage
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  e.target.reset();
+
+  displayTasks()
 }
 
 window.addEventListener('load', () => {
@@ -32,21 +54,46 @@ window.addEventListener('load', () => {
   nameOwner.value = username;
   helloUsername.textContent = "de " + username;
 
-  newTaskForm.addEventListener('submit', e => {
-    e.preventDefault();
-
-    //creating tasks array
-    const task = {
-      name: e.target.elements.task.value,
-      priority: e.target.elements.priority.checked,
-      done: false,
-      createdAt: new Date().getTime()
-    }
-    //adding a task to the array
-    tasks.push(task);
-
-    //saving the array localStorage
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    e.target.reset();
-  })
+  displayTasks()
 })
+
+function displayTasks() {
+  const tasksList = document.querySelector('#task-template').content;
+  let tasksContainer = document.querySelector('.tasks-container');
+
+
+  // verify if there are tasks in array 'tasks' (global variable)
+  if (tasks.length > 0) {
+
+    tasksContainer.innerHTML = '';
+
+    tasks.forEach(t => {
+      const task = tasksList.querySelector('.task').cloneNode(true);
+      const taskName = t.name;
+      const taskDone = t.done; // true/false
+      const taskCheckbox = task.querySelector('.task-done');
+
+      task.querySelector('.task-name').value = taskName;
+
+      if (taskDone) {
+        taskCheckbox.classList.add('done');
+      }
+
+      taskCheckbox.addEventListener('change', (e) => {
+        t.done = e.target.checked;
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
+        if (t.done) {
+          taskCheckbox.classList.add('done');
+        } else {
+          taskCheckbox.classList.remove('done');
+        }
+      })
+
+      tasksContainer.appendChild(task)
+    })
+
+  } else {
+    console.log('El array de tareas está vacío.');
+  }
+}
